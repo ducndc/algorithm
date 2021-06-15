f5b6f1dea337fcc8e3d38132d9af642b8c5f47b6
Ducndc <ducndc5699@gmail.com>
2021-06-15 07:40:40 +0700

    Update imolement quick sort

---
+++ BasicSort/ImplementQuickSort.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void swap(int *a, int *b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+int partition(int arr[], int l, int h)
+{
+	int x = arr[h];
+	int i = l - 1;
+
+	for (int j = l; j <= h-1; j++)
+	{
+		if (arr[j] <= x)
+		{
+			i++;
+			swap(&arr[i], &arr[j]);
+		}
+	}
+
+	swap(&arr[i+1], &arr[h]);
+
+	return i+1;
+}
+
+void quickSortIterative(int arr[], int l, int h)
+{
+	int stack[h - l + 1];
+	int top = -1;
+
+	stack[++top] = l;
+	stack[++top] = h;
+
+	while (top >= 0)
+	{
+		h = stack[top--];
+		l = stack[top--];
+
+		int p = partition(arr, l, h);
+
+		if (p-1 > l)
+		{
+			stack[++top] = p + 1;
+			stack[++top] = h;
+		}
+	}
+}
+
+printArray(int arr[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
+
+int main(void)
+{
+	int arr[] = {4, 3, 5, 2, 1, 3, 2, 3};
+	int n = sizeof(arr) / sizeof(*arr);
+
+	quickSortIterative(arr, 0, n-1);
+
+	printArray(arr, n);
+
+	return 0;
+}
